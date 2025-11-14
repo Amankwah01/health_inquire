732680317fb4d3da8b68a25e4a3665f195c61bc4
Amankwah01 <emmanuelamankwah540@gmail.com>
2025-11-14 09:42:00 +0000

    updated About Us section

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -17,12 +17,10 @@ import { Textarea } from "./ui/textarea";
 function AboutUs() {
   return (
     <div id="about-us" className="w-full flex justify-center p-10">
-      <div className="flex flex-col h-full px-10 mb-5 rounded-2xl">
-        <div className="flex flex-col gap-y-3 w-full h-full">
-          <h1 className="w-full text-2xl font-bold text-green-500 mb">
-            About Us
-          </h1>
-          <div className="w-full flex flex-col justify-center items-start text-black">
+      <div className="flex flex-col h-full mb-5 rounded-2xl">
+        <div className="flex flex-col gap-y-3  w-full h-full">
+          <h1 className="w-full text-4xl font-bold mb">About Us</h1>
+          <div className="w-full flex flex-col justify-center items-start text-black px-10">
             <h1 className=" w-full text-2xl font-bold text-green-500 my-2">
               Aim
             </h1>
@@ -39,7 +37,7 @@ function AboutUs() {
               debitis eum provident. Itaque.
             </p>
           </div>
-          <div className="w-full flex flex-col justify-center items-start text-black">
+          <div className="w-full flex flex-col justify-center items-start text-black px-10">
             <h1 className=" w-full text-2xl font-bold text-green-500 my-2">
               Our Story
             </h1>
